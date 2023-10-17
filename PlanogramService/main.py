adde64a8448cf434f373f3ffaed1e279c7402b96
Bastian1110 <chebimora02@gmail.com>
2023-10-17 17:43:24 -0600

    basic planogram api

---
+++ PlanogramService/main.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+database = MongoClient(DATABASE_URL)["NoLinger"]
+planogramCollection = database["Planograms"]
+
+app = Flask("No Linger Planogram Service")
+CORS(app)
+
+
+@app.route("/getPlanograms")
+def get_planograms():
+    planograms = []
+    response = planogramCollection.find({})
+    for planogram in response:
+        planogram["_id"] = str(planogram["_id"])
+        planograms.append(planogram)
+    return jsonify({"planograms": planograms}), 200
+
+
+if __name__ == "__main__":
+    app.run(port="8082", debug=True)
