3d6e60216ce87c6774b40b3f7c6a19406fb5477d
Adrian101-hnd <69586718+Adrian101-hnd@users.noreply.github.com>
2023-11-06 16:24:44 -0600

    First user class

---
+++ app/models/users.py
@@ -0,0 +1,33 @@
+from pymongo.collection import Collection
+import logging
+
+class Users:
+
+    def __init__(self,id, name, email, phone,store_id, collection: Collection) -> None:
+        self.id = id
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.store_id = store_id
+        self.collection = collection
+
+    def to_dic(self) -> dict:
+        return{
+            '_id':self.id,
+            'Nombre':self.name,
+            'Email':self.email,
+            'Telefono':self.phone,
+            'Tienda_id':self.store_id,
+            'Rol':self.role
+        }
+    def insert_user(self) -> bool:
+        try:
+            self.collection.insert_one(self.to_dic())
+            return True
+        except Exception as e:
+            logging.error(e)
+            return False
+        
+    
+
+    
